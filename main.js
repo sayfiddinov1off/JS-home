f8a53f3ba13f90804fd2fd8e7ad2b97e0f09daf4
Azamat <105981809+sayfiddinov1off@users.noreply.github.com>
2022-11-23 19:13:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+// let firstName = prompt("Ismingizni kiriting");
+// let surname = prompt("Familyangizni kiritng");
+// let age = prompt("Yoshingizni kiriting");
+// let edu = prompt("O'qish joyingizni kiriting");
+
+
+
+
+
+
+// // let obj = {
+// //     name: firstName,
+// //     lastName: surname,
+// //     ages: age,
+// //     school: edu
+// // }
+// // console.log(obj);
+
+
+// if(age <= 7){
+//     console.log("Siz bog'cha bolasisiz");
+// }
+// else if(age <= 16){
+//     console.log("Siz maktabda o'qiysiz");
+// }
+// else if(age <= 30){
+//     console.log("Siz universitetda o'qiysiz");
+// }
+// else{
+//     console.log("Siz ishllaysiz");
+    
+// }
+
+
+
+
+let color = prompt("Svitafor rangini kiriting")
+
+if(color == "qizil"){
+    console.log("sariq");
+}
+else if(color == "sariq"){
+    console.log("yashil");
+}
+else if(color == "yashil"){
+    console.log("qizil");
+}
+else{
+    console.log("siz svitafor rangini kiritmadingiz");
+}
